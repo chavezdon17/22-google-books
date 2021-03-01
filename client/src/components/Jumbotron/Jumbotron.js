22d0dd7f4ceeaef85a5c192cfb33bcfb63722db0
Donovan Chavez <chavezdon17@gmail.com>
2021-03-01 01:12:18 -0600

    jumbotron

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-4 text-center myText">Search for books</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
